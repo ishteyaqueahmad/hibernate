3e9562911bb5675947853b4526c5d4812c4f6780
ishteyaque <ahmad,shteyaque966@gmail.com>
2019-04-15 20:22:40 +0530

    hibernate app updated

---
+++ src/org/hibernate/dto/UserDetails.java
@@ -0,0 +1,25 @@
+package org.hibernate.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetails {
+	
+	@Id
+	private int id;
+	private String userName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
